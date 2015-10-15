86921f172f790c04e88eb187091d0bde48e15c5b
jack_you <jackyou@savy.tw>
2015-10-15 11:22:59 +0800

    change banner

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,32 @@
-angular.module("getbookmarks.services", ["ngResource"]).
-    factory('Story', function ($resource) {
-        var Story = $resource('/api/v1/stories/:storyId', {storyId: '@id'});
-        Story.prototype.isNew = function(){
-            return (typeof(this.id) === 'undefined');
-        }
-        return Story;
-    });
+var app =angular.module('autoApp' , ['ngResource']);
+
+    app.controller('SignUpController', ['$scope', '$resource', function($scope,$resource) {
+        var backEndServer = $resource('http://192.168.1.104\\:5000/auth/register');
+        $scope.submit =function(){
+         console.log('submit');
+         backEndServer.save({}, {
+              "email": "test1123@gmail.com", 
+               "password": "123456", 
+               "password_confirm": "123456"
+              }, function(response) {
+        // Handle a successful response
+                 console.log('success');
+                 console.log(response);
+             }, function(response) {
+                 console.log(response);
+                console.log('fail');
+        // Handle a non-successful response
+        });
+     };
+
+    }]);
+    // .factory('Story', function ($resource) {
+    //     var Story = $resource('/api/v1/stories/:storyId', {storyId: '@id'});
+    //     Story.prototype.isNew = function(){
+    //         return (typeof(this.id) === 'undefined');
+    //     }
+    //     return Story;
+    // });
 
 angular.module("getbookmarks", ["getbookmarks.services"]).
     config(function ($routeProvider) {
