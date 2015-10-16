ab887e0d6b8c7dd06d81f0da15dea43b6286ec6d
jack_you <jackyou@savy.tw>
2015-10-16 17:56:00 +0800

    add mask

--- js/main.js
+++ js/main.js
@@ -83,13 +83,13 @@
 
 				$window.on('load', function() {
 
-					// $banner.scrollwatch({
-					// 	delay:		0,
-					// 	range:		0.5,
-					// 	anchor:		'top',
-					// 	on:			function() { $header.addClass('alt reveal'); },
-					// 	off:		function() { $header.removeClass('alt'); }
-					// }); //mark by test
+					$banner.scrollwatch({
+						delay:		0,
+						range:		0.5,
+						anchor:		'top',
+						on:			function() { $header.addClass('alt reveal'); },
+						off:		function() { $header.removeClass('alt'); }
+					}); //mark by test
 
 				});
 
